1d1fff6dad5c5d4ca4756a555febde0086e947d9
hj41790 <hj41790@naver.com>
2023-07-05 17:03:47 +0900

    cycle 2

--- UnitTest/test.cpp
+++ UnitTest/test.cpp
@@ -7,3 +7,8 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess("12"), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess("12s"), invalid_argument);
+}
