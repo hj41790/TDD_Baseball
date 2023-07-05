869721fcf4b2d892ce221174433f21f2ec83083d
hj41790 <hj41790@naver.com>
2023-07-06 08:55:32 +0900

    cycle 4 - refactoring

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -41,7 +41,12 @@ public:
 	GuessResult guess(const string& guessNumber)
 	{
 		assertIllegalArgument(guessNumber);
-		return { true, 3, 0 };
+
+		if (guessNumber == question) {
+			return { true, 3, 0 };
+		}
+
+		return { false, 0, 0 };
 	}
 
 	explicit Baseball(const string& question) : question(question) {}
